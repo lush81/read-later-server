594f09092e3513d12c75c03fe96303f710e49c25
lush81 <lush@i.ua>
2015-04-02 16:24:03 +0000

    server

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
 var express = require('express');
 var app = express();
+var read = require('node-readability');
+
+var Firebase = require('firebase');
+var fb = new Firebase("https://final-project-lush.firebaseio.com/");
+
+var bodyParser = require('body-parser');
+app.use(bodyParser());
+
+var cors = require('cors');
+app.use(cors());
+
+app.get('/scraper', function(req, res) {
+  var url = req.query.url;
+  read(url, function(err, article, meta) { 
+    res.status(200).jsonp({
+      url: url,
+      title: article.title,
+      content: article.content
+    })
+  })
+});
+
+app.post('/scraper', cors(), function (req, res) {
+  var url = req.body.url;
+  if (!url) {
+      return res.sendStatus(400);
+    } 
+  read(url, function(err, article, meta) {
+    fb.child("articles").push({
+      url: url,
+      title: article.title,
+      content: article.content
+    });
+  res.sendStatus(res.statusCode);                                       
+  })
+});
 
 module.exports = app;
